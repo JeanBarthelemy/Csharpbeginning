780fe292d729c619830e16de47244180869034bd
JeanBarthelemy <58735903+JeanBarthelemy@users.noreply.github.com>
2020-01-06 11:59:58 +0100

    copie du programme C#

---
+++ testMaxValues.cs
@@ -0,0 +1,13 @@
+using System;
+ class Test
+    {
+        static void Main()
+        {
+            int x = int.MaxValue;
+            long y = long.MaxValue;
+            short z = short.MaxValue;
+            long additionResult = x + y + z;
+            
+            System.Console.WriteLine(additionResult);
+        }
+    }
