174f63c4bd80062630736ff5ed52929683289254
JeanBarthelemy <58735903+JeanBarthelemy@users.noreply.github.com>
2020-01-06 12:15:24 +0100

    modif solution

--- testMaxValues.cs
+++ testMaxValues.cs
@@ -6,7 +6,7 @@ using System;
             int x = int.MaxValue;
             long y = long.MaxValue;
             short z = short.MaxValue;
-            long additionResult = x + y + z;
+            ulong additionResult = (ulong)x + (ulong)y + (ulong)z;
             
             System.Console.WriteLine(additionResult);
         }
